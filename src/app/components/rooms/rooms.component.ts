57f793125ae7911eff260a231958b66dd3d3f3ec
Mohammad Khoulani <mhmmdkholani@gmail.com>
2023-04-09 18:16:44 +0300

    Angular directives

--- src/app/components/rooms/rooms.component.ts
+++ src/app/components/rooms/rooms.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Room, RoomList } from './rooms';
 
 @Component({
   selector: 'app-rooms',
@@ -10,6 +11,39 @@ export class RoomsComponent {
   roomNumber = 12;
   available = true;
   hideRooms = false;
+  rooms: Room = { availableRooms: 3, bookedRooms: 5, totalRooms: 20 };
+
+  roomList: RoomList[] = [
+    {
+      roomNumber: 1,
+
+      roomType: 'Deluxe Room',
+      amenities: "Free Wifi, Air condition, Tv",
+      price: 200,
+      photos: "https://placehold.co/600x400",
+      checkInDate: new Date("11-nov-2021"),
+      checkOutDate: new Date("11-nov-2021"),
+    },
+    {
+      roomNumber: 2,
+      roomType: 'Deluxe Room 1',
+      amenities: "Free Wifi, Air condition, Tv",
+      price: 2200,
+      photos: "https://placehold.co/600x400",
+      checkInDate: new Date("11-nov-2021"),
+      checkOutDate: new Date("11-nov-2021"),
+    },
+    {
+      roomNumber: 3,
+
+      roomType: 'Deluxe Room 2',
+      amenities: "Free Wifi, Air condition, Tv",
+      price: 2030,
+      photos: "https://placehold.co/600x400",
+      checkInDate: new Date("11-nov-2021"),
+      checkOutDate: new Date("11-nov-2021"),
+    }
+  ];
 
   toggle() {
     this.hideRooms = !this.hideRooms;
---
+++ src/app/components/rooms/rooms.ts
@@ -0,0 +1,14 @@
+export interface Room {
+    totalRooms: number;
+    availableRooms: number;
+    bookedRooms: number;
+}
+
+export interface RoomList {
+    roomType: string; roomNumber: number;
+    amenities: string;
+    price: number;
+    photos: string;
+    checkInDate: Date;
+    checkOutDate: Date;
+}
