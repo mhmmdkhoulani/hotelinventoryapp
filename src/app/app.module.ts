7a11ee54334a5cdf863a22cc4253580dfe364486
Mohammad <mhmmdkholani@gmail.com>
2023-04-10 00:52:19 +0300

    Adding bootsrtap to project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RoomsComponent } from './components/rooms/rooms.component';
 import { StaffComponent } from './components/staff/staff.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { StaffComponent } from './components/staff/staff.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
